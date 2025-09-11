4b56a37b8a5fbbc7b1690acf1b27b6b3b9c6df2e
igarett <igarett@yandex.ru>
2025-09-11 14:30:46 +0700

    Подстветка комментариев и копирование кода

--- js/main.js
+++ js/main.js
@@ -48,9 +48,9 @@
         const switchContainer = document.createElement('div');
         switchContainer.className = 'comment-switch-container';
 
-        const switchText = document.createElement('span');
-        switchText.textContent = 'Подсветка комментариев';
-        switchContainer.appendChild(switchText);
+        // const switchText = document.createElement('span');
+        // switchText.textContent = 'Комментарии';
+        // switchContainer.appendChild(switchText);
 
         const switchLabel = document.createElement('label');
         switchLabel.className = 'comment-switch';
@@ -68,8 +68,51 @@
 
         preBlock.appendChild(switchContainer);
 
+        // --- Tooltip for comment switch ---
+        const updateTooltip = () => {
+            switchContainer.title = checkbox.checked ? 'Выкл. подсветку комментариев' : 'Вкл. подсветку комментариев';
+        };
+        updateTooltip(); // Set initial tooltip
+
         checkbox.addEventListener('change', () => {
             preBlock.classList.toggle('comments-off', !checkbox.checked);
+            updateTooltip(); // Update tooltip on change
+        });
+
+        // --- Copy code button ---
+        const copyButton = document.createElement('button');
+        copyButton.className = 'copy-code-button';
+        copyButton.title = 'Копировать код';
+        copyButton.innerHTML = `
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-clipboard" viewBox="0 0 16 16">
+                <path d="M4 1.5H3a2 2 0 0 0-2 2V14a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V3.5a2 2 0 0 0-2-2h-1v1h1a1 1 0 0 1 1 1V14a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V3.5a1 1 0 0 1 1-1h1v-1z"/>
+                <path d="M9.5 1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-3a.5.5 0 0 1-.5-.5v-1a.5.5 0 0 1 .5-.5h3zm-3-1A1.5 1.5 0 0 0 5 1.5v1A1.5 1.5 0 0 0 6.5 4h3A1.5 1.5 0 0 0 11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3z"/>
+            </svg>`;
+
+        preBlock.appendChild(copyButton);
+
+        copyButton.addEventListener('click', () => {
+            navigator.clipboard.writeText(codeBlock.textContent).then(() => {
+                copyButton.innerHTML = `
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check-lg" viewBox="0 0 16 16">
+                        <path d="M12.736 3.97a.733.733 0 0 1 1.047 0c.286.289.29.756.01 1.05L7.88 12.01a.733.733 0 0 1-1.065.02L3.217 8.384a.757.757 0 0 1 0-1.06.733.733 0 0 1 1.047 0l3.052 3.093 5.4-6.425a.247.247 0 0 1 .02-.022z"/>
+                    </svg>`;
+                copyButton.title = 'Скопировано!';
+                copyButton.classList.add('copied');
+
+                setTimeout(() => {
+                    copyButton.innerHTML = `
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-clipboard" viewBox="0 0 16 16">
+                            <path d="M4 1.5H3a2 2 0 0 0-2 2V14a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V3.5a2 2 0 0 0-2-2h-1v1h1a1 1 0 0 1 1 1V14a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V3.5a1 1 0 0 1 1-1h1v-1z"/>
+                            <path d="M9.5 1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-3a.5.5 0 0 1-.5-.5v-1a.5.5 0 0 1 .5-.5h3zm-3-1A1.5 1.5 0 0 0 5 1.5v1A1.5 1.5 0 0 0 6.5 4h3A1.5 1.5 0 0 0 11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3z"/>
+                        </svg>`;
+                    copyButton.title = 'Копировать код';
+                    copyButton.classList.remove('copied');
+                }, 2000);
+            }).catch(err => {
+                console.error('Failed to copy text: ', err);
+                copyButton.title = 'Ошибка копирования';
+            });
         });
     });
 
